ec7016208711a3f356039ecae4d9d9109008c29e
Marcelo Ecker <marcelohhecker@gmail.com>
2021-02-26 23:33:20 -0300

    ADD finalizando desafio

--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -1,14 +1,6 @@
 import styled from 'styled-components/native';
 import { FlatList } from 'react-native';
 
-interface Product {
-  id: string;
-  title: string;
-  image_url: string;
-  price: number;
-  quantity: number;
-}
-
 export const Container = styled.SafeAreaView`
   flex: 1;
   align-items: center;
@@ -21,7 +13,8 @@ export const ProductContainer = styled.View`
   flex-direction: row;
 `;
 
-export const ProductList = styled(FlatList as new () => FlatList<Product>)`
+export const ProductList = styled(FlatList)`
+  flex: 1;
   padding: 0 10px;
 `;
 
@@ -30,6 +23,7 @@ export const Product = styled.View`
   padding: 15px 10px;
   border-radius: 5px;
   margin: 5px;
+  flex: 1;
   flex-direction: row;
 `;
 
@@ -97,10 +91,8 @@ export const ActionButton = styled.TouchableOpacity`
 export const TotalProductsContainer = styled.View`
   position: absolute;
   bottom: 0px;
-
   flex-direction: row;
   background: #e83f5b;
-
   padding: 20px 40px;
   justify-content: space-between;
   align-items: center;
@@ -110,7 +102,6 @@ export const TotalProductsText = styled.Text`
   font-size: 16px;
   color: #fff;
   margin-left: 15px;
-
   flex: 1;
   font-weight: bold;
 `;
